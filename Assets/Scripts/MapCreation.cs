f6f57f0c17871ab1c9f67f991512661c2dd68f7d
qiaoxu123 <xu.qiao@intel.com>
2023-07-16 09:35:51 +0800

    Added the random objects in scene

--- Assets/Scripts/MapCreation.cs
+++ Assets/Scripts/MapCreation.cs
@@ -14,21 +14,29 @@ public class MapCreation : MonoBehaviour
     // 实例化物体 
     private void Awake() {
         // 实例化老家
-        CreateItem(item[0], new Vector3(0, -4.5f, 0), Quaternion.identity);
-        CreateItem(item[2], new Vector3(-1, -4.5f, 0), Quaternion.identity);
-        CreateItem(item[2], new Vector3(1, -4.5f, 0), Quaternion.identity);
+        CreateItem(item[0], new Vector3(0, -6, 0), Quaternion.identity);
+        CreateItem(item[2], new Vector3(-1, -6, 0), Quaternion.identity);
+        CreateItem(item[2], new Vector3(1, -6, 0), Quaternion.identity);
         for (float i = -1; i < 2; ++ i) { 
-            CreateItem(item[2], new Vector3(i, -3.5f, 0), Quaternion.identity);
+            CreateItem(item[2], new Vector3(i, -5, 0), Quaternion.identity);
         }
 
         // 实例化外围 AirWall
-        for (float i = -8.5f; i < 9.5f; ++ i) {
-            CreateItem(item[6], new Vector3(i, -5.5f, 0), Quaternion.identity);
-            CreateItem(item[6], new Vector3(i, 5.5f, 0), Quaternion.identity);
+        for (float i = -11; i < 12; ++ i) {
+            CreateItem(item[6], new Vector3(i, -7, 0), Quaternion.identity);
+            CreateItem(item[6], new Vector3(i, 7, 0), Quaternion.identity);
         }
-        for (float i = -5.5f; i < 6.5f; ++ i) {
-            CreateItem(item[6], new Vector3(-8.5f, i, 0), Quaternion.identity);
-            CreateItem(item[6], new Vector3(8.5f, i, 0), Quaternion.identity);
+        for (float i = -7; i < 8; ++ i) {
+            CreateItem(item[6], new Vector3(-11, i, 0), Quaternion.identity);
+            CreateItem(item[6], new Vector3(11, i, 0), Quaternion.identity);
+        }
+
+        //实例化地图
+        for (int i = 0; i < 20; ++ i) {
+            CreateItem(item[2], CreateRandomPosition(), Quaternion.identity);
+            CreateItem(item[3], CreateRandomPosition(), Quaternion.identity);
+            CreateItem(item[4], CreateRandomPosition(), Quaternion.identity);
+            CreateItem(item[5], CreateRandomPosition(), Quaternion.identity);
         }
     }
 
@@ -43,7 +51,7 @@ public class MapCreation : MonoBehaviour
     private Vector3 CreateRandomPosition() {
         // 限定地图边界，不让其在这里生成物体。当前地图大小为 x(-7.5, 7.5), y(-4.5, 4.5)
         while (true) {
-            Vector3 createPosition = new Vector3(Random.Range(-7.5f, 8.5f), Random.Range(-4.5f, 5.5f), 0); 
+            Vector3 createPosition = new Vector3(Random.Range(-10, 11), Random.Range(-6, 7), 0); 
             if (!HasThePosition(createPosition)) {
                 return createPosition;
             }
